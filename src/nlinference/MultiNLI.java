92e5e5ceb15bd2fd798ed76cc0fd530d373b6729
Gabor Berend <berendg@inf.u-szeged.hu>
2018-05-04 11:20:08 +0200

    makes the creation of the feature matrix on-line (i.e. the entire text and tree format of the data is not kept in the memory

--- src/nlinference/MultiNLI.java
+++ src/nlinference/MultiNLI.java
@@ -11,10 +11,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
+
 import smile.classification.LogisticRegression;
 
 /**
@@ -23,100 +26,118 @@ import smile.classification.LogisticRegression;
  */
 public class MultiNLI {
 
-    static ArrayList<SentencePair> pairs = new ArrayList<SentencePair>();
-    static ArrayList<WordAsVec> vecs = new ArrayList<WordAsVec>();
-    static FileFilter sentence = new FileFilter() {
+  static FileFilter sentence = new FileFilter() {
 
-        @Override
-        public boolean accept(File file) {
-            return file.getName().endsWith(".json") || file.getName().endsWith(".txt") || file.isDirectory();
-        }
-
-        @Override
-        public String getDescription() {
-            return "Sentences";
-        }
-    };
-    static FileFilter wordvec = new FileFilter() {
-
-        @Override
-        public boolean accept(File file) {
-            return file.getName().endsWith(".gz") || file.getName().endsWith(".txt") || file.isDirectory();
-        }
-
-        @Override
-        public String getDescription() {
-            return "Word vectors";
-        }
-    };
-
-    public static File showGUI(FileFilter filter) {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileFilter(filter);
-        chooser.showOpenDialog(null);
-        chooser.getSelectedFile();
-        return chooser.getSelectedFile();
+    @Override
+    public boolean accept(File file) {
+      return file.getName().endsWith(".json") || file.getName().endsWith(".txt") || file.isDirectory();
     }
 
-    public static void main(String args[]) {
-        File file1;
-        File file2;
-        if (args.length > 0 && args[0].equals("-gui")) {
-            file1 = showGUI(sentence);
-            file2 = showGUI(wordvec);
-        } else {
-            if (args.length > 0) {
-                file1 = new File(args[0]);
-            } else {
-                file1 = showGUI(sentence);
-            }
-            if (args.length > 1) {
-                file2 = new File(args[0]);
-            } else {
-                file2 = showGUI(wordvec);
-            }
-        }
-        if (file1 == null || file2 == null) {
-            System.out.println("No file selected");
-        }
-        try {
-            BufferedReader sreader = new BufferedReader(new InputStreamReader(new FileInputStream(file1)));
-            String line = sreader.readLine();
-            while (line != null) {
-                String sentence[] = line.split("\t");
-                int type = -1;
-                if (line.startsWith("contradiction\t")) {
-                    type = 0;
-                } else if (line.startsWith("neutral\t")) {
-                    type = 1;
-                } else if (line.startsWith("entailment\t")) {
-                    type = 2;
-                }
-                pairs.add(new SentencePair(sentence[3], sentence[4], type));
-
-                line = sreader.readLine();
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(MultiNLI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            BufferedReader vreader = new BufferedReader(new InputStreamReader(new FileInputStream(file2)));
-            String line = vreader.readLine();
-            WordAsVec.vecSize = Integer.parseInt(line.split(" ")[1]);
-            line = vreader.readLine();
-            while (line != null) {
-                vecs.add(new WordAsVec(line));
-                line = vreader.readLine();
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(MultiNLI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        double vecarray[][] = new double[WordAsVec.vecSize * WordAsVec.vecSize][pairs.size()];
-        int types[]=new int[pairs.size()];
-        for(int i=0;i<pairs.size();i++){
-            vecarray[i]=pairs.get(i).getSentencePairVec(vecs);
-            types[i]=pairs.get(i).getType();
-        }
-        LogisticRegression regression = new LogisticRegression(vecarray,types);
+    @Override
+    public String getDescription() {
+      return "Sentences";
     }
+  };
+  static FileFilter wordvec = new FileFilter() {
+
+    @Override
+    public boolean accept(File file) {
+      return file.getName().endsWith(".gz") || file.getName().endsWith(".txt") || file.isDirectory();
+    }
+
+    @Override
+    public String getDescription() {
+      return "Word vectors";
+    }
+  };
+
+  public static File showGUI(FileFilter filter) {
+    JFileChooser chooser = new JFileChooser();
+    chooser.setFileFilter(filter);
+    chooser.showOpenDialog(null);
+    chooser.getSelectedFile();
+    return chooser.getSelectedFile();
+  }
+
+  public static void main(String args[]) {
+    File file1;
+    File file2;
+    if (args.length > 0 && args[0].equals("-gui")) {
+      file1 = showGUI(sentence);
+      file2 = showGUI(wordvec);
+    } else {
+      if (args.length > 0) {
+        file1 = new File(args[0]);
+      } else {
+        file1 = showGUI(sentence);
+      }
+      if (args.length > 1) {
+        file2 = new File(args[1]);
+      } else {
+        file2 = showGUI(wordvec);
+      }
+    }
+    if (file1 == null || file2 == null) {
+      System.out.println("No file selected");
+    }
+    ArrayList<WordAsVec> vecs = new ArrayList<>();
+    try (BufferedReader vreader = new BufferedReader(new InputStreamReader(new FileInputStream(file2)))) {
+      String line = vreader.readLine();
+      WordAsVec.vecSize = Integer.parseInt(line.split(" ")[1]);
+      vecs = new ArrayList<WordAsVec>(Integer.parseInt(line.split(" ")[0]));
+      line = vreader.readLine();
+      while (line != null) {
+        vecs.add(new WordAsVec(line));
+        line = vreader.readLine();
+      }
+    } catch (IOException ex) {
+      Logger.getLogger(MultiNLI.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    System.err.format("%d vectors read in...", vecs.size());
+
+    LinkedList<double[]> featuresAsList = new LinkedList<>();
+    LinkedList<Integer> typesAsList = new LinkedList<>();
+    try (BufferedReader sreader = new BufferedReader(new InputStreamReader(new FileInputStream(file1)))) {
+      sreader.readLine(); // the first line is just the header anyway, so skip it
+      String line = sreader.readLine();
+      int lineCounter = 1;
+      while (line != null) {
+        String sentence[] = line.split("\t");
+        int type = -1;
+        if (line.startsWith("contradiction\t")) {
+          type = 0;
+        } else if (line.startsWith("neutral\t")) {
+          type = 1;
+        } else if (line.startsWith("entailment\t")) {
+          type = 2;
+        }
+        typesAsList.add(type);
+        SentencePair sentencePair = new SentencePair(sentence[3], sentence[4], type);
+        double[] featuresOfSentencePair = sentencePair.getSentencePairVec(vecs); // TODO fix this!!!
+        featuresAsList.add(featuresOfSentencePair);
+        line = sreader.readLine();
+        if (++lineCounter % 150000 == 0) {
+          break;
+        }
+        if (lineCounter % 10000 == 0) {
+          System.err.println(lineCounter + " " + line);
+        }
+      }
+    } catch (IOException ex) {
+      Logger.getLogger(MultiNLI.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    double[][] features = new double[featuresAsList.size()][WordAsVec.vecSize * WordAsVec.vecSize];
+    int i = 0;
+    for (double[] f : featuresAsList) {
+      features[i++] = f;
+    }
+    int[] types = new int[typesAsList.size()];
+    i = 0;
+    for (Integer type : typesAsList) {
+      types[i++] = type;
+    }
+    LogisticRegression regression = new LogisticRegression(features, types); // TODO handle proper model selection (e.g. cross-validation)
+    int prediction = regression.predict(features[0]);
+    System.err.println(prediction + " " + types[0]);
+  }
 }
--- src/nlinference/SentencePair.java
+++ src/nlinference/SentencePair.java
@@ -6,137 +6,143 @@
 package nlinference;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  *
  * @author Lacko
  */
-public class SentencePair{
-    private int type=-1;
+public class SentencePair {
+  private int type = -1;
 
-    private String Atokens[];
-    private String Btokens[];
+  private String Atokens[];
+  private String Btokens[];
 
-    private SentenceTree ATree;
-    private SentenceTree BTree;
+  private SentenceTree ATree;
+  private SentenceTree BTree;
 
-    private SentenceTree buildTree(String sentence) {
-        SentenceTree parent = null;
-        SentenceTree current = null;
+  private SentenceTree buildTree(String sentence) {
+    SentenceTree parent = null;
+    SentenceTree current = null;
 
-        String segments[] = sentence.split(" ");
-        for (int i = 0; i < segments.length; i++) {
-            if (segments[i].startsWith("(")) {
-                if (i + 1 < segments.length && segments[i + 1].endsWith(")")) {
-                    String word = segments[i + 1];
-                    while (word.endsWith(")")) {
-                        word = word.substring(0, word.length() - 1);
-                    }
-                    //System.out.println(segments[i+1]);
-                    current = new SentenceTree(segments[i].substring(1), word);
-                } else {
-                    current = new SentenceTree(segments[i].substring(1), null);
-                }
-                if (parent != null) {
-                    current.setParent(parent);
-                    parent.addChild(current);
-                }
-                parent = current;
-            }
-            while (segments[i].endsWith(")")) {
-                current = parent;
-                parent = current.getParent();
-                segments[i] = segments[i].substring(0, segments[i].length() - 1);
-            }
+    String segments[] = sentence.split(" ");
+    for (int i = 0; i < segments.length; i++) {
+      if (segments[i].startsWith("(")) {
+        if (i + 1 < segments.length && segments[i + 1].endsWith(")")) {
+          String word = segments[i + 1];
+          while (word.endsWith(")")) {
+            word = word.substring(0, word.length() - 1);
+          }
+          // System.out.println(segments[i+1]);
+          current = new SentenceTree(segments[i].substring(1), word);
+        } else {
+          current = new SentenceTree(segments[i].substring(1), null);
         }
-        return current;
+        if (parent != null) {
+          current.setParent(parent);
+          parent.addChild(current);
+        }
+        parent = current;
+      }
+      while (segments[i].endsWith(")")) {
+        current = parent;
+        parent = current.getParent();
+        segments[i] = segments[i].substring(0, segments[i].length() - 1);
+      }
     }
-    public int getType(){
-        return this.type;
-    }
-    public void setType(int type){
-        this.type=type;
-    }
-    public SentencePair(String Asentence, String Bsentence,int type) {
-        this.ATree = buildTree(Asentence);
-        this.BTree = buildTree(Bsentence);
+    return current;
+  }
 
-        Atokens = ATree.getSentence().split(" ");
-        Btokens = BTree.getSentence().split(" ");
-        
-        this.type=type;
-    }
-    public SentencePair(String Asentence, String Bsentence) {
-        this.ATree = buildTree(Asentence);
-        this.BTree = buildTree(Bsentence);
+  public int getType() {
+    return this.type;
+  }
 
-        Atokens = ATree.getSentence().split(" ");
-        Btokens = BTree.getSentence().split(" ");
-    }
-    float JaccardPerCharAvarge(){
-        float sum=0.0f;
-        int len=Atokens.length<Btokens.length?Atokens.length:Btokens.length;
-        for(int i=0;i<len;i++){
-            int equal=0;
-            int wlen=Atokens[i].length()<Btokens[i].length()?Atokens[i].length():Btokens[i].length();
-            for(int j=0;j<wlen;j++){
-                if(Atokens[i].charAt(j)==Btokens[i].charAt(j)){
-                    equal++;
-                }
-            }
-            sum+=((float)equal/(float)(Atokens[i].length()+Btokens[i].length()-equal));
-        }
-        int div=Atokens.length>Btokens.length?Atokens.length:Btokens.length;
-        return sum/div;
-    }
-    public double[] getSentencePairVec(ArrayList<WordAsVec> vecs){
-        WordAsVec Asentence[]=new WordAsVec[Atokens.length];
-        System.out.println("sent");
-        for(int i=0;i<Atokens.length;i++){
-            Asentence[i]=WordAsVec.find(vecs,Atokens[i]);
-        }
-        System.out.println("sent");
-        WordAsVec Bsentence[]=new WordAsVec[Btokens.length];
-        for(int i=0;i<Btokens.length;i++){
-            Bsentence[i]=WordAsVec.find(vecs,Btokens[i]);
-        }
-        System.out.println("sent");
-        WordAsVec Avec=WordAsVec.avarge(Asentence);
-        WordAsVec Bvec=WordAsVec.avarge(Bsentence);
-        double result[]=new double[WordAsVec.vecSize*WordAsVec.vecSize];
-        
-        for(int i=0;i<WordAsVec.vecSize;i++){
-            for(int j=0;j<WordAsVec.vecSize;j++){
-                result[i*WordAsVec.vecSize+j]=Avec.getWordvec()[i]*Bvec.getWordvec()[j];
-            }
-        }
-        return result;
-    }
-    float JaccardPerWord() {
-        int wordCount = Atokens.length < Btokens.length ? Btokens.length : Atokens.length;
-        int lastWordIndex = Atokens.length > Btokens.length ? Btokens.length : Atokens.length;
-        int equalCount = 0;
-        for (int i = 0; i < lastWordIndex; i++) {
-            if (Atokens[i].equals(Btokens[i])) {
-                equalCount++;
-            }
-        }
-        return (float) equalCount / ((float) (lastWordIndex + wordCount - equalCount));
-    }
+  public void setType(int type) {
+    this.type = type;
+  }
 
-    public static Comparator<SentencePair> compPerWord = new Comparator<SentencePair>(){
-        @Override
-        public int compare(SentencePair t, SentencePair t1) {
-            if (t.JaccardPerWord() < t1.JaccardPerWord()) {
-                return 1;
-            } else if (t.JaccardPerWord() > t1.JaccardPerWord()) {
-                return -1;
-            } else {
-                return 0;
-            }
+  public SentencePair(String Asentence, String Bsentence, int type) {
+    this.ATree = buildTree(Asentence);
+    this.BTree = buildTree(Bsentence);
+
+    Atokens = ATree.getSentence().split(" ");
+    Btokens = BTree.getSentence().split(" ");
+
+    this.type = type;
+  }
+
+  public SentencePair(String Asentence, String Bsentence) {
+    this.ATree = buildTree(Asentence);
+    this.BTree = buildTree(Bsentence);
+
+    Atokens = ATree.getSentence().split(" ");
+    Btokens = BTree.getSentence().split(" ");
+  }
+
+  float JaccardPerCharAvarge() {
+    float sum = 0.0f;
+    int len = Atokens.length < Btokens.length ? Atokens.length : Btokens.length;
+    for (int i = 0; i < len; i++) {
+      int equal = 0;
+      int wlen = Atokens[i].length() < Btokens[i].length() ? Atokens[i].length() : Btokens[i].length();
+      for (int j = 0; j < wlen; j++) {
+        if (Atokens[i].charAt(j) == Btokens[i].charAt(j)) {
+          equal++;
         }
-    };
-    
+      }
+      sum += ((float) equal / (float) (Atokens[i].length() + Btokens[i].length() - equal));
+    }
+    int div = Atokens.length > Btokens.length ? Atokens.length : Btokens.length;
+    return sum / div;
+  }
+
+  public double[] getSentencePairVec(ArrayList<WordAsVec> vecs) {
+    WordAsVec Asentence[] = new WordAsVec[Atokens.length];
+    System.out.println("sent");
+    for (int i = 0; i < Atokens.length; i++) {
+      Asentence[i] = WordAsVec.find(vecs, Atokens[i]);
+    }
+    System.out.println("sent");
+    WordAsVec Bsentence[] = new WordAsVec[Btokens.length];
+    for (int i = 0; i < Btokens.length; i++) {
+      Bsentence[i] = WordAsVec.find(vecs, Btokens[i]);
+    }
+    System.out.println("sent");
+    WordAsVec Avec = WordAsVec.avarge(Asentence);
+    WordAsVec Bvec = WordAsVec.avarge(Bsentence);
+    double result[] = new double[WordAsVec.vecSize * WordAsVec.vecSize];
+
+    for (int i = 0; i < WordAsVec.vecSize; i++) {
+      for (int j = 0; j < WordAsVec.vecSize; j++) {
+        result[i * WordAsVec.vecSize + j] = Avec.getWordvec()[i] * Bvec.getWordvec()[j];
+      }
+    }
+    return result;
+  }
+
+  public float JaccardPerWord() {
+    int wordCount = Atokens.length < Btokens.length ? Btokens.length : Atokens.length;
+    int lastWordIndex = Atokens.length > Btokens.length ? Btokens.length : Atokens.length;
+    int equalCount = 0;
+    for (int i = 0; i < lastWordIndex; i++) {
+      if (Atokens[i].equals(Btokens[i])) {
+        equalCount++;
+      }
+    }
+    return (float) equalCount / ((float) (lastWordIndex + wordCount - equalCount));
+  }
+
+  public static Comparator<SentencePair> compPerWord = new Comparator<SentencePair>() {
+    @Override
+    public int compare(SentencePair t, SentencePair t1) {
+      if (t.JaccardPerWord() < t1.JaccardPerWord()) {
+        return 1;
+      } else if (t.JaccardPerWord() > t1.JaccardPerWord()) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  };
+
 }
